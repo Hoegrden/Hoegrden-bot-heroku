17b36edfa88d41385913800ba70699d06be9e585
Hoegrden <37790436+Hoegrden@users.noreply.github.com>
2018-05-18 11:16:09 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,55 +8,6 @@ const prefix = '!'
 
 
 
-var http = require('http');
-
-var fs = require('fs');
-
-var express = require('express');
-
-var bodyParser = require('body-parser');
-
-var ejs = require('ejs');
-
- 
-
-var app = express();
-
-app.use(bodyParser());
-
-var router = express.Router();
-
-app.use(express.static('public'));
-
-app.use(router);
-
- 
-
-var port = process.env.PORT || 3000;
-
- 
-
-router.get("/",function(req,res){
-
- res.send("<h1>hello heroku node.js world</h1>");
-
-});
-
- 
-
-http.createServer(app).listen(port, function(){
-
- console.log('server run');
-
-});
-
-
-
-
-
-
-
-
 
 
 var count=0;
